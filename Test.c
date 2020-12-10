42cf36efddd722fb50f40c814e01312f833732ce
CNoch <wr389110442@outlook.com>
2020-12-10 10:07:47 +0800

    V 1.0.0 2020.12.10 提交代码说明文档

---
+++ Test.c
@@ -0,0 +1,17 @@
+/*
+ * @Author: Noch
+ * @Date: 2020-09-27 15:30:27
+ * @LastEditTime: 2020-09-30 09:11:57
+ * @LastEditors: Noch
+ * @Description: 测试用例
+ * @FilePath: \C\Test.c
+ * @三十功名尘与土，八千里路云和月
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("123");
+    return 0;    
+}
